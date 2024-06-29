a7da8e92b7fe2e98657c281485e791fea651d0c7
nishi240masa <masa2401106@gmail.com>
2024-06-29 18:49:42 +0900

    add:dbの型定義

---
+++ src/types/index.ts
@@ -0,0 +1,18 @@
+// ここで型を定義する
+
+// トランザクションの種類の型を定義
+export type TransactionType = "income" | "expense";
+// 収入のカテゴリーを定義
+export type IncomeCategory = "給与"|"副収入"|"お小遣い";
+// 支出のカテゴリーを定義
+export type ExpenseCategory = "食費"|"日用品"|"住居費"|"交際費"|"娯楽"|"交通費";
+
+// トランザクションの型を定義
+export interface Transaction{
+    id: string;
+    date: string;
+    amount: number;
+    content: string;
+    type: TransactionType;
+    category: ExpenseCategory | IncomeCategory;
+}
