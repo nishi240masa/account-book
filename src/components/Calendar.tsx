ad63a16ccd7f985f286b80a2c76eae37fcafa687
nishi240masa <masa2401106@gmail.com>
2024-06-30 10:22:47 +0900

    Add FullCalendar plugin and styles

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import jaLocale from "@fullcalendar/core/locales/ja";
+import "../calendar.css";
+import { EventContentArg } from "@fullcalendar/core";
 
 const Calendar = () => {
-  return (
-    <div>Calendar</div>
-  )
-}
+  const events = [
+    {
+      title: "Meeting",
+      start: "2024-06-20",
+      income: "¥1000",
+      expense: "¥500",
+      balance: "¥500",
+    },
+  ];
 
-export default Calendar
+  const renderEventContent = (eventInfo: EventContentArg) => {
+    return (
+      <div>
+        <div className="mony" id="event-income">
+          {eventInfo.event.extendedProps.income}
+        </div>
+        <div className="mony" id="event-expense">
+          {eventInfo.event.extendedProps.expense}
+        </div>
+        <div className="mony" id="event-balance">
+          {eventInfo.event.extendedProps.balance}
+        </div>
+      </div>
+    );
+  };
+  return (
+    <FullCalendar
+      locale={jaLocale}
+      plugins={[dayGridPlugin]}
+      initialView="dayGridMonth"
+      events={events}
+      eventContent={renderEventContent}
+    />
+  );
+};
+
+export default Calendar;
