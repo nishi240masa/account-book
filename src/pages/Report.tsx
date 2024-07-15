25fe5bf9431cc1fa3bf50442f72d3b7e7cd6ea0b
nishi240masa <masa2401106@gmail.com>
2024-07-15 16:02:07 +0900

    Add Paper component to Report page

--- src/pages/Report.tsx
+++ src/pages/Report.tsx
@@ -1,17 +1,22 @@
-import { Grid } from "@mui/material";
+import { Grid, Paper } from "@mui/material";
 import React from "react";
 
 const Report = () => {
+  const commonPaperStyle = {
+    height: { xs: "auto", md: "400px" },
+    display: "flex",
+    flexDirection: "column",
+  };
   return (
     <Grid container spacing={2}>
       <Grid item xs={12}>
         日付
       </Grid>
       <Grid item xs={12} md={4}>
-        カテゴリグラフ
+        <Paper sx={commonPaperStyle}>カテゴリグラフ</Paper>
       </Grid>
       <Grid item xs={12} md={8}>
-        棒グラフ
+        <Paper sx={commonPaperStyle}>棒グラフ</Paper>
       </Grid>
       <Grid item xs={12}>
         テーブル
