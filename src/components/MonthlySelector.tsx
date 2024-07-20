3d30650caa7cfce81766d4e5086289569c72f111
nishi240masa <masa2401106@gmail.com>
2024-07-20 12:05:05 +0900

    Add MonthlySelector component with date picker

--- src/components/MonthlySelector.tsx
+++ src/components/MonthlySelector.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import { Box, Button } from "@mui/material";
+import React from "react";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+// date-fns v3なのでドキュメントの2個目のコードを参考にする
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFnsV3";
+// date-fnsの日本語化のためのimport
+import { ja } from "date-fns/locale";
 
 const MonthlySelector = () => {
   return (
-    <div>MonthlySelector</div>
-  )
-}
+    <LocalizationProvider
+      dateAdapter={AdapterDateFns}
+      adapterLocale={ja}
+      // monthAndYearが存在しないため、コメントアウト
+      // DatePickerの方で違う形で指定する
+      // dateFormats={{ monthAndYear: "yyyy年 MM月" }}
+    >
+      <Box
+        sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
+      >
+        {/* variant="contained"は塗りつぶし */}
+        <Button color={"error"} variant="contained">
+          先月
+        </Button>
+        {/* viewsはカレンダーに表示させたい情報を選べる */}
+        {/* formatは表示の仕方を加工できる */}
+        <DatePicker
+          label="年月を選択"
+          sx={{ mx: 2, backgroundColor: "white" }}
+          views={["year", "month"]}
+          format="yyyy/MM"
+          // hidden: false,を指定することで、toolbarを表示させる
+          slotProps={{
+            toolbar: {
+              toolbarFormat: "yyyy/MM",
+              hidden: false,
+            },
+            // dateFormats={{ monthAndYear: "yyyy年 MM月" }}代わり
+            calendarHeader: { format: "yyyy/MM" },
+          }}
+        />
+        <Button color={"primary"} variant="contained">
+          次月
+        </Button>
+      </Box>
+    </LocalizationProvider>
+  );
+};
 
-export default MonthlySelector
+export default MonthlySelector;
--- src/pages/Report.tsx
+++ src/pages/Report.tsx
@@ -15,7 +15,6 @@ const Report = () => {
     <Grid container spacing={2}>
       <Grid item xs={12}>
         <MonthlySelector />
-        日付
       </Grid>
       <Grid item xs={12} md={4}>
         <Paper sx={commonPaperStyle}>
