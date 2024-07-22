f96a38b267e7f0f4a88d5f660b36e0b6d6ca5e1d
nishi240masa <masa2401106@gmail.com>
2024-07-22 16:10:02 +0900

    Add image upload functionality to ImageStorage component

--- src/pages/ImageStorage.tsx
+++ src/pages/ImageStorage.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { ref, uploadBytes } from "firebase/storage";
 import { storage } from "../firebase";
 
+// 画像をアップロードするコンポーネント
 function ImageStorage() {
   const [image, setImage] = useState<File>();
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -9,11 +10,13 @@ function ImageStorage() {
       setImage(e.target.files[0]);
     }
   };
+
+  // formの送信処理
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     try {
       if (!image) return;
       const imageRef = ref(storage, image.name);
-
+      // 画像をアップロードする処理を書く
       uploadBytes(imageRef, image).then(() => {
         console.log("Uploaded a file!");
       });
